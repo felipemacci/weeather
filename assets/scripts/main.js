00fe6d2962e91b075f9cd38bf627658eccb987f0
Felipe Macci <felipemacci@gmail.com>
2022-08-03 12:38:55 -0300

    made basic weather and city request system

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,49 @@
-import Logo from '../scripts/logo.js'
-
+const currentCity= document.querySelector('#weather .weather__city')
+const currentTemperature = document.querySelector('#weather .weather__degrees')
+const currentClimate = document.querySelector('#weather .weather__climate')
 const forecastNavigation = document.querySelector('.weather__forecast')
 const forecastExpander = document.querySelector('.weather__forecast > a')
 const forecastList = document.querySelector('.weather__forecast-menu .weather__forecast-list')
+
+const changeCityText = city => {
+    currentCity.innerText = city
+}
+
+const changeTemperatureText = degrees => {
+    currentTemperature.innerText = degrees
+}
+
+const changeClimateText = climate => {
+    currentClimate.innerText = climate
+}
+
+const sendForecastReqs = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+        const lat = position.coords.latitude
+        const lon = position.coords.longitude
+        const city = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=bcfaab6c78be19a12909f0a481f4da85`
+        const info = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=hourly,daily&appid=bcfaab6c78be19a12909f0a481f4da85`
+
+        fetch(city)
+        .then(res => res.json())
+        .then(data => {
+            const local = data[0].name
+            changeCityText(local)
+        })
+        
+        fetch(info)
+        .then(res => res.json())
+        .then(data => {
+            const temperature = data.current.temp.toFixed() + '°'
+            const climate = data.current.weather[0].main
+
+            changeTemperatureText(temperature)
+            changeClimateText(climate)
+        })
+    })
+}
+
+sendForecastReqs()
 
 const toggleForecastExpansion = () => {
     if(forecastNavigation.getAttribute('aria-expanded') == 'false') {
@@ -27,5 +68,3 @@ forecastList.addEventListener('wheel', e => {
 
     e.preventDefault()
 })
-
-customElements.define('app-logo', Logo)
