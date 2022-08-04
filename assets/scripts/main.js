b78ed4ec4700a26b6fb0752c2e6b08560127493c
Felipe Macci <felipemacci@gmail.com>
2022-08-03 22:47:26 -0300

    added city search bar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,70 +1,72 @@
-const currentCity= document.querySelector('#weather .weather__city')
-const currentTemperature = document.querySelector('#weather .weather__degrees')
-const currentClimate = document.querySelector('#weather .weather__climate')
+const cityForm = document.querySelector('header form')
+const cityInput = document.querySelector('header form input')
+
 const forecastNavigation = document.querySelector('.weather__forecast')
-const forecastExpander = document.querySelector('.weather__forecast > a')
+const forecastExpander = document.querySelector('.weather__forecast .weather__open-weather-menu')
 const forecastList = document.querySelector('.weather__forecast-menu .weather__forecast-list')
 
-const changeCityText = city => {
-    currentCity.innerText = city
+const currentCity = document.querySelector('#weather .weather__city')
+const currentTemperature = document.querySelector('#weather .weather__degrees')
+const currentClimate = document.querySelector('#weather .weather__climate')
+
+const mainDisplay = [ currentCity, currentTemperature, currentClimate ]
+
+const changeInfo = (info, data) => {
+    if(info == currentCity && !cityInput.value) {
+        cityInput.value = data
+    }
+
+    info.innerText = data
 }
 
-const changeTemperatureText = degrees => {
-    currentTemperature.innerText = degrees
+const updateInfos = (data) => {
+    const local = data.name
+    const temperature = data.main.temp.toFixed() + '°'
+    const climate = data.weather[0].main
+
+    const currentInfo = [ local, temperature, climate ]
+
+    currentInfo.forEach((_, index) => changeInfo(mainDisplay[index], currentInfo[index]))
 }
 
-const changeClimateText = climate => {
-    currentClimate.innerText = climate
-}
-
-const sendForecastReqs = () => {
+const setLocateInfos = () => {
     navigator.geolocation.getCurrentPosition(position => {
         const lat = position.coords.latitude
         const lon = position.coords.longitude
-        const city = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=bcfaab6c78be19a12909f0a481f4da85`
-        const info = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=hourly,daily&appid=bcfaab6c78be19a12909f0a481f4da85`
+        const currentLocation = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=bcfaab6c78be19a12909f0a481f4da85`
 
-        fetch(city)
+        fetch(currentLocation)
         .then(res => res.json())
         .then(data => {
-            const local = data[0].name
-            changeCityText(local)
-        })
-        
-        fetch(info)
-        .then(res => res.json())
-        .then(data => {
-            const temperature = data.current.temp.toFixed() + '°'
-            const climate = data.current.weather[0].main
+            const city = `https://api.openweathermap.org/data/2.5/weather?q=${data[0].name}&units=metric&appid=bcfaab6c78be19a12909f0a481f4da85`
 
-            changeTemperatureText(temperature)
-            changeClimateText(climate)
+            fetch(city)
+            .then(res => res.json())
+            .then(data => updateInfos(data))
         })
     })
 }
 
-sendForecastReqs()
-
 const toggleForecastExpansion = () => {
     if(forecastNavigation.getAttribute('aria-expanded') == 'false') {
         forecastNavigation.setAttribute('aria-expanded', 'true')
-        forecastExpander.querySelector('i').style.transform = 'rotate(180deg)'
-        forecastExpander.querySelector('span').style.display = 'none'
     } else {
         forecastNavigation.setAttribute('aria-expanded', 'false')
-        forecastExpander.querySelector('i').style.transform = 'rotate(0)'
-        forecastExpander.querySelector('span').style.display = 'block'
     }
 }
 
-forecastExpander.addEventListener('click', e => {
-    e.preventDefault()
-    toggleForecastExpansion()
-})
-
 forecastList.addEventListener('wheel', e => {
-    if(e.deltaY > 0) forecastList.scrollLeft += 100
-    else forecastList.scrollLeft -= 100
-
-    e.preventDefault()
+    e.deltaY > 0 ? forecastList.scrollLeft += 100 : forecastList.scrollLeft -= 100
 })
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const city = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&appid=bcfaab6c78be19a12909f0a481f4da85`
+
+    fetch(city)
+    .then(res => res.json())
+    .then(data => updateInfos(data))
+})
+
+setLocateInfos()
