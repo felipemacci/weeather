b4e1840c7ef49842f989ba7abf9ed6f02d8b337e
Felipe Macci <felipemacci@gmail.com>
2022-08-15 17:47:55 -0300

    commenting the code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -15,6 +15,7 @@ const currentClimate = document.querySelector('#weather .weather__climate')
 const mainSection = document.getElementById('weather')
 const mainDisplay = [ currentCity, currentTemperature, currentClimate ]
 
+// Starting the app with the information provided by the browser's geolocation
 const setLocateInfos = () => {
     navigator.geolocation.getCurrentPosition(position => {
         const lat = position.coords.latitude
@@ -23,10 +24,12 @@ const setLocateInfos = () => {
         const currentLocation = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=bcfaab6c78be19a12909f0a481f4da85`
         const currentClimateData = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=bcfaab6c78be19a12909f0a481f4da85`
 
+        // Getting the city name through the API
         axios.get(currentLocation)
         .then(res => {
             const cityName = res.data[0].name
 
+            // Getting and directing the weather information obtained through the API
             axios.get(currentClimateData)
             .then(res => {
                 const data = res.data
@@ -40,6 +43,7 @@ const setLocateInfos = () => {
     })
 }
 
+// Directing the city's weather data to its proper functions
 const handleForecastInfo = city => {
     const cityTime = getCurrentTime(city)
 
@@ -48,6 +52,7 @@ const handleForecastInfo = city => {
     createForecastListItems(city, cityTime)
 }
 
+// Getting the current city time based on the time zone
 const getCurrentTime = data => {
     let currentTime = new Date().toLocaleString('pt-BR', { timeZone: data.timezone })
     currentTime = parseInt(currentTime.split(' ')[1].slice(0, 2))
@@ -55,6 +60,7 @@ const getCurrentTime = data => {
     return currentTime
 }
 
+// Updating main display information
 const updateInfos = data => {
     const local = data.cityName || data.name
     const temperature = (data.main ? data.main : data.current).temp.toFixed() + '°'
@@ -65,6 +71,7 @@ const updateInfos = data => {
     currentInfo.forEach((_, index) => changeInfo(mainDisplay[index], currentInfo[index]))
 }
 
+// Changing specific display information
 const changeInfo = (info, data) => {
     if(info == currentCity && !cityInput.value) {
         cityInput.value = data
@@ -73,6 +80,7 @@ const changeInfo = (info, data) => {
     info.innerText = data
 }
 
+// Setting the background based on the city time
 const setTimeBackground = time => {
     if(time >= 18) {
         mainSection.style.backgroundImage = 'url("../assets/images/night.jpg")'
@@ -85,6 +93,7 @@ const setTimeBackground = time => {
     }
 }
 
+// Creating each of the items in the weather forecast lists
 const createForecastListItems = (city, cityTime) => {
     let todayCounter = 0
     let tomorrowCounter = 0
@@ -92,6 +101,7 @@ const createForecastListItems = (city, cityTime) => {
     todayForecastList.innerHTML = null
     tomorrowForecastList.innerHTML = null
 
+    // For each remaining hour of the day a forecast will be created
     for(let i = cityTime; i < 24; i++) {
         const forecastedDegrees = getForecastedDegrees(city, todayCounter)
         const icon = getDueForecastIcon(city, todayCounter)
@@ -105,6 +115,7 @@ const createForecastListItems = (city, cityTime) => {
         todayCounter++
     }
 
+    // For the 24 hours of the next day, a forecast will be created
     for(let i = todayCounter; i < (24 + todayCounter); i++) {
         const forecastedDegrees = getForecastedDegrees(city, i)
         const icon = getDueForecastIcon(city, i)
@@ -119,18 +130,22 @@ const createForecastListItems = (city, cityTime) => {
     }
 }
 
+// Getting the city temperature based on the given time
 const getForecastedDegrees = (city, hoursLeft) => {
     return city.hourly[hoursLeft].temp.toFixed()
 }
 
+// Getting the city weather related icon at the given time
 const getDueForecastIcon = (city, hoursLeft) => {
     return `http://openweathermap.org/img/wn/${city.hourly[hoursLeft].weather[0].icon}.png`
 }
 
+// Getting the formatted city time based on the given time
 const getForecastTime = hour => {
     return (hour < 10 ? `0${hour}:00` : `${hour}:00`)
 }
 
+// Adding an item to the forecast list based on the model and data provided
 const addListItem = (list, info) => {
     const template = `
     <li>
@@ -143,6 +158,7 @@ const addListItem = (list, info) => {
     list.innerHTML += template
 }
 
+// Handling API errors
 const setError = error => {
     if(error.response) {
         switch(error.response.status) {
@@ -158,6 +174,7 @@ const setError = error => {
     }
 }
 
+// Toggling the list state (shown or not)
 const toggleForecastExpansion = () => {
     if(forecastNavigation.getAttribute('aria-expanded') == 'false') {
         forecastNavigation.setAttribute('aria-expanded', 'true')
@@ -166,17 +183,41 @@ const toggleForecastExpansion = () => {
     }
 }
 
-forecastList.forEach(list => {
-    list.addEventListener('wheel', e => {
-        e.deltaY > 0 ? list.scrollLeft += 100 : list.scrollLeft -= 100
+// Initializing forecast list navigation
+const initListNavigation = () => {
+    // This allows the user to scroll through the lists with the mouse wheel
+    forecastList.forEach(list => {
+        list.addEventListener('wheel', e => {
+            e.deltaY > 0 ? list.scrollLeft += 100 : list.scrollLeft -= 100
+        })
     })
-})
 
+    // This allows the user to switch between the "today" and "tomorrow" lists
+    forecastListChanger.forEach(option => {
+        option.addEventListener('click', e => {
+            e.preventDefault()
+    
+            forecastListChanger.forEach(item => item.parentElement.removeAttribute('active'))
+            e.target.parentElement.setAttribute('active', '')
+    
+            if(e.target.innerText != 'Today') {
+                todayForecastList.style.display = 'none'
+                tomorrowForecastList.style.display = 'flex'
+            } else {
+                todayForecastList.style.display = 'flex'
+                tomorrowForecastList.style.display = 'none'
+            }
+        })
+    })
+}
+
+// When the user enters the desired city, the application will follow its flow based on its information
 cityForm.addEventListener('submit', e => {
     e.preventDefault()
 
     const location = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&appid=bcfaab6c78be19a12909f0a481f4da85`
 
+    // Getting simple city information based on the name entered in the input
     axios.get(location)
     .then(res => {
         const data = res.data
@@ -187,6 +228,7 @@ cityForm.addEventListener('submit', e => {
 
         const climateData = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=bcfaab6c78be19a12909f0a481f4da85`
         
+        // Getting and directing the weather information obtained through the API
         axios.get(climateData)
         .then(res => {
             const data = res.data
@@ -199,22 +241,6 @@ cityForm.addEventListener('submit', e => {
     .catch(error => setError(error))
 })
 
-forecastListChanger.forEach(option => {
-    option.addEventListener('click', e => {
-        e.preventDefault()
-
-        forecastListChanger.forEach(item => item.parentElement.removeAttribute('active'))
-        e.target.parentElement.setAttribute('active', '')
-
-        if(e.target.innerText != 'Today') {
-            todayForecastList.style.display = 'none'
-            tomorrowForecastList.style.display = 'flex'
-        } else {
-            todayForecastList.style.display = 'flex'
-            tomorrowForecastList.style.display = 'none'
-        }
-    })
-})
-
 setLocateInfos()
 setTimeBackground()
+initListNavigation()
